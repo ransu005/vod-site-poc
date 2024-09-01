6b443d3201019986ae4d10144deaf17bcbdffa39
ransu005 <lancelotlazaro05@gmail.com>
2024-09-01 19:01:35 +0800

    Adds mock data and bindings for home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { VideosService } from '../../services/videos.service';\
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    RouterLink, 
+    RouterLink,
     RouterOutlet,
-    NavigationComponent],
+    NavigationComponent,],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  styles: [`:host {display: block;}`]
 })
 export class HomeComponent {
+  videos: any;
+
+  constructor(videosService: VideosService) {
+    videosService.getVideos().subscribe(vid => {
+        this.videos = vid
+      }
+    );
+  }
 }
+
--- src/app/services/videos.service.ts
+++ src/app/services/videos.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideosService {
 
-  constructor() { }
+  data = {
+    videos: [
+      {
+        "id": 1234,
+        "title": "Test Title 1",
+        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.",
+        "category": "Fiction"
+      }
+    ]
+  };
+
+  constructor() { 
+  }
+
+  getVideos() {
+    return of(this.data.videos);
+  }
+
 }
